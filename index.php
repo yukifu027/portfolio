cef43b3c9d7d0076897b71b400b2e03959177ac7
yukifu027 <63824425+yukifu027@users.noreply.github.com>
2020-07-12 23:16:05 +0900

    大まかなマークアップ

--- index.php
+++ index.php
@@ -6,6 +6,51 @@
     <title>Yuki's Portfolio</title>
   </head>
   <body>
-    <?php echo "<h2>helloworld</h2>"; ?>
+    <header>
+      <div class="header">
+        <div class="contents">
+          <a href="#">WORKS</a>
+          <a href="#">FEATURE</a>
+          <a href="#">ABOUT</a>
+          <a href="#">SKILL</a>
+          <a href="#">CONTACT</a>
+        </div>
+      </div>
+    </header>
+
+    <div class="main">
+      <div class="title">
+        <div class="title__myname">YUKI FUKUI</div>
+        <div class="title__subtitle">PORTFOLIO</div>
+      </div>
+      <div class="works">
+        <div class="works__subtitle">WORKS</div>
+        <div class="works__intro">これまでに作成したWebアプリ・IOSアプリです。<br>TECHCAMP（プログラミングスクール）での課題制作も含んでおります。</div>
+      </div>
+      <div class="feature">
+        <div class="feature__subtitle">FEATURE</div>
+        <div class="feature__intro">１サイトの解説</div>
+      </div>
+      <div class="about">
+        <div class="about__subtitle">ABOUT</div>
+      </div>
+      <div class="skill">
+        <div class="skill__subtitle">SKILL</div>
+        <div class="skill__intro">未経験ですが頑張ります！</div>
+      </div>
+      <div class="contact">
+        <div class="contact__subtitle">CONTACT</div>
+        <div class="contact__intro">お問い合わせは、SNSかメールでお願いいたします。</div>
+      </div>
+      <div class="returnToTop">
+        <a href="#">トップへもどる</a>
+      </div>
+    </div>
+
+    <footer>
+      <div class="footer">
+        Copyright©YUKI FUKUI. All Rights Reserved.
+      </div>
+    </footer>
   </body>
 </html>
